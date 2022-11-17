b02aa6657d9f3b4ad08d7d0dda399119e27aacc7
SysMLinJavaModeler <54550824+SysMLinJavaModeler@users.noreply.github.com>
2022-11-17 12:44:04 -0600

    Add files via upload

---
+++ CableStayedBridge/ExecuteModel.bat
@@ -0,0 +1,6 @@
+java.exe ^
+-Djava.util.logging.config.file=logging.properties ^
+-Dfile.encoding=UTF-8 ^
+-p ".\bin;..\SysMLinJava\bin" ^
+-XX:+ShowCodeDetailsInExceptionMessages ^
+-m CableStayedBridge/cablestayedbridge.CableStayedBridgeDomain
